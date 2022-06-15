0b55ceb42805acf8942efca33ef105699edd0452
SatyamSitam <satyamsct@gmail.com>
2022-06-15 20:59:02 +0530

    update

--- Sign up & login page/js/login.js
+++ Sign up & login page/js/login.js
@@ -1,21 +1,23 @@
 document.querySelector("#button").addEventListener("click",getData);
 
-let userData = JSON.parse(localStorage.getItem("signupData"));
+let userLogin = JSON.parse(localStorage.getItem("signupData"));
 
 function getData(){
         let loginEmail = document.querySelector("#email").value;
         let loginPassword = document.querySelector("#password").value;
         let count=0;
-    for(let i=0; i<userData.length; i++){
-        if(userData[i].userEmail==loginEmail&& userData[i].userPassword==loginPassword){
+    for(let i=0; i<userLogin.length; i++){
+        if(userLogin[i].userEmail==loginEmail&& userLogin[i].userPassword==loginPassword){
             count++;
         }
     }
     if(count===1){
         alert("You are login sucessfully.");
+        localStorage.setItem("userLogin",JSON.stringify(loginEmail));
         window.location.href="/index.html";
     }else{
         alert("Your email or password may wrong.");
+        
     }
 }
 
